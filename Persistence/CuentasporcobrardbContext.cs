77c27f5fa4402e8f5b2321e4584dd36427eb4e1c
Lunext <eurengabriel@gmail.com>
2023-02-23 01:26:18 -0500

    Well

---
+++ Persistence/CuentasporcobrardbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CuentasPorCobrar.Shared;
+
+public partial class CuentasporcobrardbContext : DbContext
+{
+    public virtual DbSet<Document> Documents { get; set;}
+    public CuentasporcobrardbContext()
+    {
+    }
+
+    public CuentasporcobrardbContext(DbContextOptions<CuentasporcobrardbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=cuentasporcobrardb;Username=postgres;Password=Euren002");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
