e7b1fbf5a9cd9693d0e0ce326b3226460a9c9406
Lunext <eurengabriel@gmail.com>
2023-02-22 23:50:47 -0500

    Uploading classes

---
+++ BusinessLogic/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; 
+
+namespace CuentasPorCobrar.Shared; 
+
+public class Customer
+{
+    [Key]
+    public int CustomerId { get; set; }
+
+    [Required]
+    [MaxLength(32)]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    [MaxLength(13)]
+    public string Identification { get; set; } = null!;
+
+
+    [Column(TypeName ="money")]
+    public decimal CreditLimit { get; set;}
+     
+    public bool IsAvailable { get; set;}
+
+
+    
+}
+
---
+++ BusinessLogic/Document.cs
@@ -0,0 +1,12 @@
+﻿namespace CuentasPorCobrar.Shared;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; 
+
+public class Document
+{
+    [Key]
+
+
+}
+
