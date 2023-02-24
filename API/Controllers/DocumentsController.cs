931afad528d284f07e1ee56e825fd6970a9a921c
Lunext <eurengabriel@gmail.com>
2023-02-23 23:03:30 -0500

    Uploading first crud

---
+++ API/Controllers/DocumentsController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using API.Repositories;
+using CuentasPorCobrar.Shared;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DocumentsController : ControllerBase
+{
+    private readonly IDocumentRepository repo; 
+
+    //constructor injects repository registered in Program 
+
+    public DocumentsController(IDocumentRepository repo)
+    {
+        this.repo=repo; 
+    }
+
+
+    [HttpGet]
+    [ProducesResponseType(200, Type=typeof(IEnumerable<Document>))]
+    public async Task<IEnumerable<Document>> GetDocuments()
+    {
+        return await repo.RetrieveAllAsync(); 
+    }
+
+
+
+    //GET: api/documents[id]
+    [HttpGet("{id}", Name =nameof(GetDocument))] //named route
+    [ProducesResponseType(200,Type=typeof(Document))]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> GetDocument(int id)
+    {
+        Document? document = await repo.RetrieveAsync(id);
+
+        
+        return document is null ? NotFound() : Ok(document); 
+    }
+
+    //POST: api/documents
+    //Body: Document(JSON,XML)
+    [HttpPost]
+    [ProducesResponseType(201, Type= typeof(Document))]
+    [ProducesResponseType(400)]
+    public async Task<IActionResult> Create([FromBody] Document document)
+    {
+        if (document is null) return BadRequest(); //400
+
+        Document? addedDoument = await repo.CreateAsync(document);
+
+        return addedDoument is null ? BadRequest("Repository failed to create Document.")
+            :CreatedAtRoute(routeName:nameof(GetDocument), 
+            routeValues: new {id= addedDoument.DocumentId},
+            value:addedDoument); 
+    }
+
+    //PUT: api/documents/[id]
+    //BODY: Document (JSON,XML)
+    [HttpPut("{id}")]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> Update(int id, [FromBody] Document document)
+    {
+        if(document is null || document.DocumentId !=id) return BadRequest();
+
+
+
+        Document? existing = await repo.RetrieveAsync(id);
+
+        if (existing is null) return NotFound();
+        await repo.UpdateAsync(id, document);
+
+        return new NoContentResult(); // 204 No content
+    }
+    //DELETE: api/documents/[id]
+    [HttpDelete("{id}")]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> Delete(int id)
+    {
+        Document? existing = await repo.RetrieveAsync(id);
+        if (existing is null) return NotFound(); //404 Resource Not Found
+
+        bool? deleted = await repo.DeleteAsync(id);
+
+        //sHORT CIRCUIT AND 
+        return deleted.HasValue && deleted.Value ?
+             new NoContentResult() 
+             : BadRequest($"Document {id} was found but failed to delete"); 
+
+    }
+
+
+}
+
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,60 @@
+using CuentasPorCobrar.Shared;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using static System.Console;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using API.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCuentasContext();
+
+builder.Services.AddControllers(options =>
+{
+    WriteLine("Default ouput formatters:");
+    foreach (IOutputFormatter formatter in options.OutputFormatters)
+    {
+        OutputFormatter? mediaFormatter =  formatter as OutputFormatter;
+        if (mediaFormatter is null)
+        {
+            WriteLine($" {formatter.GetType().Name}");
+        }
+        else //OutputFormatter class has SupportedMediaTypes
+        {
+            WriteLine(" {0}, Media types: {1}",
+                mediaFormatter.GetType().Name,
+                string.Join(", ",
+                mediaFormatter.SupportedMediaTypes));
+
+        }
+    }
+})
+    .AddXmlDataContractSerializerFormatters()
+    .AddXmlSerializerFormatters();
+
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen(doc =>
+{
+    doc.SwaggerDoc("v1", new()
+    {
+        Title="Cuentas Por Cobrar Service API",
+        Version="v1"
+    });
+
+
+});
+builder.Services.AddScoped<IDocumentRepository, DocumentRepository>(); 
+
 
 var app = builder.Build();
 
@@ -13,13 +62,44 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(doc =>
+    {
+        doc.SwaggerEndpoint("/swagger/v1/swagger.json", "Cuentas por Cobrar Service API Version 1");
+
+        doc.SupportedSubmitMethods(new[]
+        {
+            SubmitMethod.Get, SubmitMethod.Post,
+            SubmitMethod.Put, SubmitMethod.Delete
+        });
+    });
 }
 
+
 app.UseHttpsRedirection();
+
+
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+
+try
+{
+    var context = services.GetRequiredService<CuentasporcobrardbContext>();
+    context.Database.Migrate(); 
+
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error ocurred during migration"); 
+
+}
+
 
 app.Run();
