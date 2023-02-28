70b4284cadb0452b289556ea779070747cecb06f
Lunext <eurengabriel@gmail.com>
2023-02-28 10:35:45 -0500

    Controller done, date needs to be fixed

---
+++ API/Controllers/AccountingEntriesController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CuentasPorCobrar.Shared;
+using API.Repositories;
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountingEntriesController : ControllerBase
+{
+    private readonly IAccountingEntryRepository repo;
+
+
+    //constructor injects repository registered in Program 
+
+    public AccountingEntriesController(IAccountingEntryRepository repo)
+    {
+        this.repo=repo;
+    }
+
+
+    [HttpGet]
+    [ProducesResponseType(200, Type = typeof(IEnumerable<AccountingEntry>))]
+    public async Task<IEnumerable<AccountingEntry>> GetAccountingEntries()
+    {
+        return await repo.RetrieveAllAsync();
+    }
+
+    //GET: api/accountingEntries[id]
+    [HttpGet("{id}", Name = nameof(GetAccountingEntry))]
+    [ProducesResponseType(200, Type = typeof(AccountingEntry))]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> GetAccountingEntry(int id)
+    {
+        AccountingEntry? accountingEntry = await repo.RetrieveAsync(id);
+
+        return accountingEntry is null ? NotFound() : Ok(accountingEntry); 
+    }
+
+    //POST: api/accountingEntries
+    //Body: AccountingEntry(JSON,XML)
+    [HttpPost]
+    [ProducesResponseType(2011,Type=typeof(AccountingEntry))]
+    [ProducesResponseType(400)]
+    public async Task<IActionResult> Create([FromBody] AccountingEntry accountingEntry)
+    {
+        if (accountingEntry is null) return BadRequest();
+
+        AccountingEntry? addedEntry = await repo.CreateAsync(accountingEntry);
+
+        return addedEntry is null ? BadRequest("Repository failed to create accounting entry")
+            : CreatedAtRoute(routeName: nameof(GetAccountingEntries),
+            routeValues: new { id = addedEntry.AccountingEntryId },
+            value: addedEntry); 
+    }
+
+
+    //PUT: api/accountingEntries/[id]
+    //BODY:AccountingEntry(JSON,XML)
+    [HttpPut("{id}")]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> Update(int id, [FromBody] AccountingEntry accountingEntry)
+    {
+        if (accountingEntry is null || accountingEntry.AccountingEntryId!=id) return BadRequest();
+
+        AccountingEntry? existing = await repo.RetrieveAsync(id);
+
+        if (existing is null) return NotFound();
+        await repo.UpdateAsync(id, accountingEntry);
+
+        return new NoContentResult(); //204 No content 
+    }
+
+    //DELETE: api/accountingEntries/[id]
+    [HttpDelete("{id}")]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> Delete(int id)
+    {
+        AccountingEntry? existing = await repo.RetrieveAsync(id);
+        if (existing is null) return NotFound();
+
+        bool? deleted = await repo.DeleteAsync(id);
+
+        //SHORT CIRCUIT AND 
+        return deleted.HasValue && deleted.Value ?
+            new NoContentResult()
+            : BadRequest($"Document {id} was found but failed to delete"); 
+        
+    }
+
+}
